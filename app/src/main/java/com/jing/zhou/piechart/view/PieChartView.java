a14e1a93e0b524af4bb247023ec298d9a540bafc
hbhd <zhou.jing@houdezhihui.com>
2017-08-29 09:43:51 +0800

    添加线条的绘画和标记的绘画,但是实现效果具体不是很好

--- app/src/main/java/com/jing/zhou/piechart/view/PieChartView.java
+++ app/src/main/java/com/jing/zhou/piechart/view/PieChartView.java
@@ -1,12 +1,15 @@
 package com.jing.zhou.piechart.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.jing.zhou.piechart.R;
 import com.jing.zhou.piechart.bean.PieData;
 
 import java.util.ArrayList;
@@ -30,6 +33,10 @@ public class PieChartView extends View {
     // 画笔
     private Paint mPaint = new Paint();
     private float value;
+    private boolean hasDividingLine;//是否需要分割线
+    private boolean showSign;//是否需要标识
+    private int dividingLineColor;//分割线颜色
+    private float dividingLineAngle;//分割线的角度
 
     public PieChartView(Context context) {
         this(context, null);
@@ -41,10 +48,19 @@ public class PieChartView extends View {
 
     public PieChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initConfig(context, attrs);
+    }
+
+    private void initConfig(Context context, AttributeSet attrs) {
         //设置绘画模式
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         //设置抗锯齿
         mPaint.setAntiAlias(true);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PieChartView);
+        hasDividingLine = typedArray.getBoolean(R.styleable.PieChartView_dividing_line, false);
+        showSign = typedArray.getBoolean(R.styleable.PieChartView_show_sign, true);
+        dividingLineColor = typedArray.getColor(R.styleable.PieChartView_dividing_line_color, Color.WHITE);
+        dividingLineAngle = typedArray.getDimension(R.styleable.PieChartView_dividing_line_angle, 1);
     }
 
     @Override
@@ -68,10 +84,11 @@ public class PieChartView extends View {
         for (int i = 0; i < mData.size(); i++) {
             PieData pie = mData.get(i);
             mPaint.setColor(pie.getColor());
-            canvas.drawArc(rect, currentStartAngle, pie.getAngle(), true, mPaint);
+            canvas.drawArc(rect, currentStartAngle, pie.getAngle() - 1, true, mPaint);
+            drawLineAndText(r, currentStartAngle, pie.getAngle(), mPaint, canvas,pie.getValue(),pie.getName());
             currentStartAngle += pie.getAngle();
-        }
 
+        }
 
     }
 
@@ -132,6 +149,25 @@ public class PieChartView extends View {
             pieData.setAngle(percentage * 360);
             sumAngle += percentage * 360;
         }
+
+    }
+
+    /**
+     * 划指示线还有标记百分比和名字等
+     */
+    private void drawLineAndText(float radius, float stopAngle, float angle, Paint paint, Canvas canvas,float value,String name) {
+        //确定直线开始的坐标
+        float startX = (float) (radius * (Math.cos(Math.toRadians(stopAngle + angle / 2))));
+        float startY = (float) (radius * (Math.sin(Math.toRadians(stopAngle + angle / 2))));
+        float stopX = (float) ((radius + 50) * (Math.cos(Math.toRadians(stopAngle + angle / 2))));
+        float stopY = (float) ((radius + 50) * (Math.sin(Math.toRadians(stopAngle + angle / 2))));
+        paint.setStrokeWidth(5);
+        canvas.drawLine(startX, startY, stopX, stopY, paint);
+        paint.setTextSize(50);
+        if (stopAngle+angle/2>90){
+            canvas.drawText(name,stopX-50,stopY-50,paint);
+        }
+        canvas.drawText(name, stopX, stopY, paint);
 
     }
 
